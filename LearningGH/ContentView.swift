28fe62821d6becb034b19710c13b9d70acd985e1
Jacob Steirer <jxs1773@miami.edu>
2021-05-24 09:21:32 -0400

    First Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Jake Steirer on 5/24/21.
+//  Copyright © 2021 Jake Steirer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Margaux!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
