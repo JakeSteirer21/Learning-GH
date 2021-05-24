9768bb1a04eb3879e9f3e785687fb2da02cbf4f6
Jacob Steirer <jxs1773@miami.edu>
2021-05-24 09:40:19 -0400

    Add MyModel to the project

---
+++ LearningGH/MyModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyModel.swift
+//  LearningGH
+//
+//  Created by Jake Steirer on 5/24/21.
+//  Copyright © 2021 Jake Steirer. All rights reserved.
+//
+
+import Foundation
+
+struct MyModel {
+    
+    var name: String?
+}
